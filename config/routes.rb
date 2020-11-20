57db2e28f18a7d3076ab20e30831422c2cd9abc0
Bertrand GENDRIN <b.gendrin76@gmail.com>
2020-11-20 12:03:22 +0100

    end coding before css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :cocktails
-  resources :doses, only: [:new, :create]
+   root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create, :new]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
